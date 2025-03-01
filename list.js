a18e432b530ac8c3670f1b0cddce76e300339921
AasthaJain76 <147072673+AasthaJain76@users.noreply.github.com>
2025-03-01 22:06:07 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,92 @@
+let allMusic = [
+    {
+        name: "Dewaana Hai Dekho",
+        artist:"Sonu Nigam",
+        img: "Dewaana.jpeg",
+        src:"Deewana.mp3"
+    },
+    {
+        name: "Savera",
+        artist:"Iqlipse Nova",
+        img: "Savera.jpeg",
+        src:"savera.mp3"
+    },
+    {
+        name: "Nadaaniyan",
+        artist:"Akshath",
+        img: "Nadaaniyan.jpeg",
+        src:"Nadaaniyan.mp3"
+    },
+    {
+        name: "Kinni Kinni",
+        artist:"Diljit Dosanjh",
+        img: "Kinni Kinni.jpeg",
+        src:"music4.mp3"
+    },
+    {
+        name: "Namo Namo",
+        artist:"Amit Trivedi",
+        img: "Namo Namo.jpeg",
+        src:"Namo Namo.mp3"
+    },
+    {
+        name: "Sunflower",
+        artist:"Post Malone,Swae Lee",
+        img: "Sunflower.jpeg",
+        src:"music9.mp3"
+    },
+    {
+        name: "Jo Tum Mere Ho",
+        artist:"Anuv Jain",
+        img: "Jo Tum Mere ho.jpeg",
+        src:"music10.mp3"
+    },
+    {
+        name: "Night Changes",
+        artist:"One Direction",
+        img: "Night Changes.jpeg",
+        src:"music11.mp3"
+    },
+    {
+        name: "Subhanallah",
+        artist:"Pritam",
+        img: "Subhanallah.jpeg",
+        src:"Subhanallah.mp3"
+    },
+    {
+        name: "Tumhare Hi Rahenge Hum",
+        artist:"Varun Jain",
+        img: "Tumhare.jpeg",
+        src:"Tumhare.mp3"
+    },
+    {
+        name: "There's Nothing Holdin me back",
+        artist:"Some one",
+        img: "Nothing.jpeg",
+        src:"music13.mp3"
+    },
+    {
+        name: "Attention",
+        artist:"Charlie Puth",
+        img: "Attention.jpeg",
+        src:"music14.mp3"
+    },
+    {
+        name: "Hai apna Dil to awara",
+        artist:"Kishore kumar",
+        img: "Hai apna Dil to awara.jpeg",
+        src:"music15.mp3"
+    },
+    {
+        name: "Dil to Bachha hai Ji",
+        artist:"Kishore kumar",
+        img: "Dil.jpeg",
+        src:"music16.mp3"
+    },
+    {
+        name: "Sweetheart",
+        artist:"Dev Negi",
+        img: "Sweetheart.jpeg",
+        src:"Sweetheart.mp3"
+    },
+];
---
+++ scipt.js
@@ -0,0 +1,153 @@
+const container = document.querySelector(".container"),
+  musicImg = document.querySelector(".img-area img"),
+  musicArtist = document.querySelector(".song-detalies .artist"),
+  progressArea = container.querySelector(".progress-area"),
+  progressBar = progressArea.querySelector(".progress-bar"),
+  playPauseBtn = container.querySelector(".play-pause"),
+  prevBtn = container.querySelector("#prev"),
+  nextBtn = container.querySelector("#next"),
+  mainAudio = container.querySelector("#main-audio"),
+  musicName = document.querySelector(".song-detalies .name");
+
+let musicIndex = 1;
+let isPlaying = false;
+
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+});
+function loadMusic(indexNumb) {
+  musicName.innerHTML = allMusic[indexNumb].name;
+  musicArtist.innerHTML = allMusic[indexNumb].artist;
+  musicImg.src = allMusic[indexNumb].img;
+  mainAudio.src = allMusic[indexNumb].src;
+}
+console.log(musicName);
+console.log(musicImg);
+
+function playMusic() {
+  isPlaying = true;
+  playPauseBtn.querySelector("span").innerHTML = "pause";
+
+  mainAudio.play();
+}
+
+function pauseMusic() {
+  isPlaying = false;
+  playPauseBtn.querySelector("span").innerHTML = "play_arrow";
+
+  mainAudio.pause();
+}
+playPauseBtn.addEventListener("click", () => {
+  isPlaying ? pauseMusic() : playMusic();
+});
+
+function nextMusic() {
+  musicIndex++;
+  musicIndex > allMusic.length - 1
+    ? (musicIndex = 0)
+    : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+}
+nextBtn.addEventListener("click", () => {
+  nextMusic();
+});
+
+function prevMusic() {
+  musicIndex--;
+  musicIndex < 0
+    ? (musicIndex = allMusic.length - 1)
+    : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+}
+prevBtn.addEventListener("click", () => {
+  prevMusic();
+});
+
+const repeatBtn = container.querySelector("#repeat-plist");
+
+repeatBtn.addEventListener("click", () => {
+  let getText = repeatBtn.innerHTML;
+  switch (getText) {
+    case "repeat":
+      repeatBtn.innerHTML = "repeat_one";
+      repeatBtn.setAttribute("title", "Song looped");
+      break;
+    case "repeat_one":
+      repeatBtn.innerHTML = "shuffle";
+      repeatBtn.setAttribute("title", "Playback shuffled");
+      break;
+    case "shuffle":
+      repeatBtn.innerHTML = "repeat";
+      repeatBtn.setAttribute("title", "Playlist looped");
+      break;
+  }
+});
+mainAudio.addEventListener("ended", () => {
+  let getText = repeatBtn.innerText;
+  switch (getText) {
+    case "repeat":
+      nextMusic();
+      break;
+    case "repeat_one":
+      mainAudio.currentTime = 0;
+      loadMusic(musicIndex);
+      playMusic();
+      break;
+    case "shuffle":
+      let randIndex = Math.floor(Math.random() * allMusic.length);
+      do {
+        randIndex = Math.floor(Math.random() * allMusic.length);
+      } while (musicIndex == randIndex);
+      {
+        musicIndex = randIndex;
+        loadMusic(musicIndex);
+        playMusic();
+        break;
+      }
+  }
+});
+mainAudio.addEventListener("timeupdate", (e) => {
+  const currentTime = e.target.currentTime;
+  const duration = e.target.duration;
+  let progressWidth = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressWidth}%`;
+  let musicCurrentTime = container.querySelector(".current-time"),
+    musicDuration = container.querySelector(".max-duration");
+
+  mainAudio.addEventListener("loadeddata", () => {
+    const interval = setInterval(() => {
+      const _elapsed = mainAudio.currentTime;
+      musicCurrentTime.innerHTML = formatTime(_elapsed);
+    }, 1000);
+    const _duration = mainAudio.duration;
+    musicDuration.innerHTML = formatTime(_duration);
+    mainAudio.addEventListener("ended", () => {
+      clearInterval(interval);
+    });
+  });
+});
+progressArea.addEventListener("click", (e) => {
+  let progressWidth = progressArea.clientWidth;
+  let clickedOffsetX = e.offsetX;
+  let songDuration = mainAudio.duration;
+
+  mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+  playMusic();
+});
+
+function formatTime(time) {
+  if (time && !isNaN(time)) {
+    const minutes =
+      Math.floor(time / 60) < 10
+        ? `0${Math.floor(time / 60)}`
+        : Math.floor(time / 60);
+    const seconds =
+      Math.floor(time % 60) < 10
+        ? `0${Math.floor(time % 60)}`
+        : Math.floor(time % 60);
+    return `${minutes}:${seconds}`;
+  }
+  return "00:00";
+}
